ad37e0f0fea4fc53617357b002af649dd8b7644b
YarinKeren <jarin4378@gmail.com>
2023-09-06 10:39:58 +0200

    started working on notes

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,14 @@
+const { useEffect, useState } = React
+import { NoteList } from '../cmps/NoteList.jsx'
+import { noteService } from '../services/note.service.js'
 
 export function NoteIndex() {
-    return <div>note app</div>
+  const [notes, setNotes] = useState(null)
+
+  useEffect(() => {
+    noteService.getNotes().then(setNotes)
+  }, [])
+
+  if (!notes) return <div>Loading...</div>
+  return <section className='note-index'>{<NoteList notes={notes} />}</section>
 }
