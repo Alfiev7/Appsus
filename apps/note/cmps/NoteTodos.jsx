be512b38213aae071d663204eaaad018dfcc3804
YarinKeren <jarin4378@gmail.com>
2023-09-07 01:49:00 +0200

    Added features - removing a todo showing remove button on hover

--- apps/note/cmps/NoteTodos.jsx
+++ apps/note/cmps/NoteTodos.jsx
@@ -17,6 +17,19 @@ export function NoteTodos({ id, createdAt, isPinned, style, info, type }) {
     })
   }
 
+  function onRemoveTodo(index) {
+    noteService
+      .get(id)
+      .then(note => {
+        note.info.todos.splice(index, 1)
+        return noteService.save(note)
+      })
+      .then(setTodos(prevTodos => prevTodos.filter((_, idx) => idx !== index)))
+      .catch(err => {
+        console.error("Couldn't remove todo:", err)
+      })
+  }
+
   function onUpdateTxt({ target: { textContent: newTxt } }, index) {
     noteService.updateNoteContent(id, type, newTxt, null, index)
   }
@@ -49,6 +62,7 @@ export function NoteTodos({ id, createdAt, isPinned, style, info, type }) {
               <a
                 className='material-icons-outlined icon'
                 onClick={() => handleOnChange(index, todo)}
+                title='mark as done'
               >
                 check_box_outline_blank
               </a>
@@ -56,18 +70,25 @@ export function NoteTodos({ id, createdAt, isPinned, style, info, type }) {
               <a
                 className='material-icons-outlined icon'
                 onClick={() => handleOnChange(index, todo)}
+                title='unmark as done'
               >
                 check_box
               </a>
             )}
             <span
-              className={todo.isDone ? 'done' : ''}
+              className={todo.isDone ? 'todo-txt done' : 'todo-txt'}
               onInput={ev => onUpdateTxt(ev, index)}
               contentEditable
               suppressContentEditableWarning={true}
             >
               {todo.txt}
             </span>
+            <a
+              className='material-icons-outlined icon icon-remove'
+              onClick={() => onRemoveTodo(index)}
+            >
+              close
+            </a>
           </div>
         </div>
       ))}
