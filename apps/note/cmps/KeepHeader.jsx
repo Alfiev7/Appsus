74c8b9341ec519d6c6a380c072f5fc8f99b5517a
YarinKeren <jarin4378@gmail.com>
2023-09-09 11:43:24 +0200

    app navigating initial

--- apps/note/cmps/KeepHeader.jsx
+++ apps/note/cmps/KeepHeader.jsx
@@ -1,9 +1,13 @@
+import { AppsMenu } from '../../../cmps/AppsMenu.jsx'
 import { noteService } from '../services/note.service.js'
 const { useState } = React
+const { useNavigate } = ReactRouterDOM
 
 export function KeepHeader({ onSetFilterBy }) {
   const [selectedIcon, setSelectedIcon] = useState('')
+  const [isMenuExpanded, setIsMenuExpanded] = useState(false)
   const icons = noteService.getIcons()
+  const navigate = useNavigate()
 
   function handleFilterByIcon(icon) {
     if (selectedIcon === icon) {
@@ -33,26 +37,35 @@ export function KeepHeader({ onSetFilterBy }) {
     ))
   }
 
+  function handleNavigate(path) {
+    navigate(path)
+  }
+
   return (
-    <div className='keep-header'>
-      <div className='keep-header-left'>
-        <i className='fas fa-bars'></i>
-        <img src='./assets/img/keep-logo.png' alt='' />
-        <h1 className='keep-header-name'>Keep</h1>
-      </div>
-
-      <div className='keep-header-middle'>
-        <div className='search-input-container'>
-          <i className='material-symbols-outlined'>search</i>
-          <input placeholder='Search notes' type='text' onChange={handleFilterByText} className='searchHeader' />
+    <React.Fragment>
+      <div className='keep-header'>
+        <div className='keep-header-left'>
+          <i className='fas fa-bars'></i>
+          <img src='./assets/img/keep-logo.png' alt='' />
+          <h1 className='keep-header-name'>Keep</h1>
         </div>
-        <div className='icons-container'>{renderIcons()}</div>
-      </div>
 
-      <div className='keep-header-right'>
-        <i className='material-symbols-outlined'>settings</i>
-        <img src='./assets/img/user-yarin.jpeg' alt='' />
+        <div className='keep-header-middle'>
+          <div className='search-input-container'>
+            <i className='material-symbols-outlined'>search</i>
+            <input placeholder='Search notes' type='text' onChange={handleFilterByText} className='searchHeader' />
+          </div>
+          <div className='icons-container'>{renderIcons()}</div>
+        </div>
+
+        <div className='keep-header-right'>
+          <i className='material-symbols-outlined icon-apps' onClick={() => setIsMenuExpanded(!isMenuExpanded)}>
+            apps
+          </i>
+          <img src='./assets/img/user-yarin.jpeg' alt='' />
+        </div>
       </div>
-    </div>
+      {isMenuExpanded && <AppsMenu handleNavigate={handleNavigate} />}
+    </React.Fragment>
   )
 }
