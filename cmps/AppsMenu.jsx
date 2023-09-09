00d70ca1cfa9924a8b0f587c1da06ce9f780c999
YarinKeren <jarin4378@gmail.com>
2023-09-09 15:00:11 +0200

    navigate to home

--- cmps/AppsMenu.jsx
+++ cmps/AppsMenu.jsx
@@ -1,7 +1,7 @@
 export function AppsMenu({ handleNavigate }) {
   return (
     <div className='apps-menu'>
-      <img src='assets/img/home.png' onClick={() => handleNavigate('/home')} alt='' />
+      <img src='assets/img/home.png' onClick={() => handleNavigate('/')} alt='' />
       <img src='assets/img/gmail.png' onClick={() => handleNavigate('/mail')} alt='' />
       <img src='assets/img/keep-logo.png' alt='' onClick={() => handleNavigate('/note')} />
       <img src='assets/img/book.png' alt='' onClick={() => handleNavigate('/book')} />
