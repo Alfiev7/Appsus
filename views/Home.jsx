140a02eabfea30c558abb73844822233069b53f1
alfiev7 <hoopsnfries@gmail.com>
2023-09-09 19:00:36 +0300

    homeMQ

--- views/Home.jsx
+++ views/Home.jsx
@@ -9,8 +9,8 @@ export function Home() {
     <section className='home'>
       <div className='home-header'>
         <h1 className='home-main-name'>Alfsus</h1>
-        <h1 className='home-main-header'>Your home, to everything</h1>
-        <h4 className='home-sub-header'>Welcome home, we are glad to see you here</h4>
+        <h1 className='home-main-header'>Your home to everything.</h1>
+        <h4 className='home-sub-header'>Welcome home, we are glad to see you here again.</h4>
       </div>
       <img className='phones' src='assets/img/two-phones.png' alt='' />
       {/* <img className='iphone-notes' src='assets/img/iphone-notes.png' alt='' /> */}
@@ -24,19 +24,3 @@ export function Home() {
   )
 }
 
-// export function AppHeader() {
-//   return (
-//     <header className='app-header'>
-//       <Link to='/'>
-//         <h3>LOGO!</h3>
-//       </Link>
-//       <nav>
-//         <NavLink to='/'>Home</NavLink>
-//         <NavLink to='/about'>About</NavLink>
-//         <NavLink to='/book'>Books</NavLink>
-//         <NavLink to='/mail'>Mail</NavLink>
-//         <NavLink to='/note'>Note</NavLink>
-//       </nav>
-//     </header>
-//   )
-// }
