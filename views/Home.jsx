58d8925ab83622b7527172a4530c32ec4d4096b9
YarinKeren <jarin4378@gmail.com>
2023-09-09 16:14:51 +0200

    started home page

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,8 +1,14 @@
 export function Home() {
   return (
     <section className='home'>
-      <h1>Alfsus - Your home, to everything</h1>
-      <h3>Welcome home, we are glad to see you here</h3>
+      <div className='home-header'>
+        <h1 className='home-main-header'>Alfsus - Your home, to everything</h1>
+        <h4 className='home-sub-header'>Welcome home, we are glad to see you here</h4>
+      </div>
+      <div className='img-container'>
+        <img className='iphone-notes' src='assets/img/iphone-notes.png' alt='' />
+        <img className='iphone-email' src='assets/img/iphone-email.png' alt='' />
+      </div>
       {/* <img src='../assets/img/phone1.png' alt='' />
       <img src='../assets/img/phone2.png' alt='' />
       <img src='../assets/img/phone3.png' alt='' />
