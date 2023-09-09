7dbbdfdce2edb8867a90fd4ec7ceb90f0a09c28f
alfiev7 <hoopsnfries@gmail.com>
2023-09-09 13:14:47 +0300

    MqFixes

--- apps/mail/cmps/SideBarComponent.jsx
+++ apps/mail/cmps/SideBarComponent.jsx
@@ -4,7 +4,7 @@ export function SideBarComponent({ icon, title, number, onClick, isActive }) {
         <div className={`SideBarComponent ${isActive ? 'active' : ''}`} onClick={onClick}>
             <i className={icon}></i>
             <h3>{title}</h3>
-            <p>{number}</p>
+            <h4>{number}</h4>
         </div>
     )
 }
--- cmps/AppsMenu.jsx
+++ cmps/AppsMenu.jsx
@@ -1,10 +1,10 @@
 export function AppsMenu({ handleNavigate }) {
   return (
     <div className='apps-menu'>
-      <img src='../../../assets/img/home.png' onClick={() => handleNavigate('/home')} alt='' />
-      <img src='../../../assets/img/gmail.png' onClick={() => handleNavigate('/mail')} alt='' />
-      <img src='../../../assets/img/keep-logo.png' alt='' onClick={() => handleNavigate('/note')} />
-      <img src='../../../assets/img/book.png' alt='' onClick={() => handleNavigate('/book')} />
+      <img src='/../../../assets/img/home.png' onClick={() => handleNavigate('/home')} alt='' />
+      <img src='/../../../assets/img/gmail.png' onClick={() => handleNavigate('/mail')} alt='' />
+      <img src='/../../../assets/img/keep-logo.png' alt='' onClick={() => handleNavigate('/note')} />
+      <img src='/../../../assets/img/book.png' alt='' onClick={() => handleNavigate('/book')} />
     </div>
   )
 }
