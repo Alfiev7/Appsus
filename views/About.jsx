639709abf2aedaefcb39e6f5b147d45a231fec8c
alfiev7 <hoopsnfries@gmail.com>
2023-09-09 23:36:47 +0300

    about us

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,48 @@
 
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
-    </section>
+    return (
+
+
+        <section className="about">
+
+            <div className="profilecontainers">
+
+
+                <div className="yarin">
+                    <img src="assets/img/yarinnew.png" alt="" />
+                    <h1>Yarin Keren</h1>
+
+                    <div className="icons">
+                    <a target='_blank' href="https://www.linkedin.com/in/yarinkeren"><i className="fa-brands fa-linkedin"></i></a>
+                    <a target='_blank' href="https://github.com/yarinkeren"><i className="fa-brands fa-github"></i></a>
+                    </div>
+                        
+                    
+                </div>
+
+                <div className="alfie">
+                    <img src="assets/img/alfie.png" alt="" />
+                    <h1>Alfie Alfandary</h1>
+
+                    <div className="icons">
+                    <a target='_blank' href="https://www.linkedin.com/in/alfie-alfandary-9a1b33222/"><i className="fa-brands fa-linkedin"></i></a>
+                    <a target='_blank' href="https://github.com/alfiev7"><i className="fa-brands fa-github"></i></a>
+                    </div>
+
+
+                </div>
+
+
+
+
+
+            </div>
+        </section>
+
+
+
+
+
+
+    )
 }
